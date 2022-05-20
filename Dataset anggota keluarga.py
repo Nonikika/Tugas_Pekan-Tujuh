78f08d5b1403d4e90afd696147ed37f884c49a81
Nonik Ika Nurfadila <102129796+Nonikika@users.noreply.github.com>
2022-05-20 13:03:27 +0700

    Update Dataset anggota keluarga.py

--- Dataset anggota keluarga.py
+++ Dataset anggota keluarga.py
@@ -1,4 +1,4 @@
 # Tugas_Pekan-Tujuh
 import pandas as pd 
-pizza = {'Nama': ['Amir', 'Fadli', 'Fatya'], 'Nilai': [7, 8, 9] }
- pizza_df = pd.DataFrame(pizza) pizza_df
+pizza = {'Nama': ['Amir', 'Fadli', 'Fatya'], 'Tinggi Badan': [120, 145, 165], 'Berat Badan':[20, 40, 65] }
+pizza_df = pd.DataFrame(pizza) pizza_df
